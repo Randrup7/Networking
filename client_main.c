d75832e4ea43631657149b496b8b6815035d4392
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2024-06-19 21:56:07 +0200

    changed main test function to print file descriptor

--- client_main.c
+++ client_main.c
@@ -1,7 +1,10 @@
 #include "client/client_socket.h"
 #include "server/server_socket.h"
+#include "stdio.h"
 
 int main(int argc, char *argv[]) {
 
-  return 0;
+	int client_fd = open_clientfd(argv[1], argv[2]);
+	printf("%d\n", client_fd);
+	return 0;
 }
