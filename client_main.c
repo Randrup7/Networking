5e7c78499659d98cd88682b2c72d199e14429399
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2024-06-28 23:00:22 +0200

    Finished main client and server file

--- client_main.c
+++ client_main.c
@@ -1,10 +1,51 @@
 #include "client/client_socket.h"
-#include "server/server_socket.h"
-#include "stdio.h"
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXLINE 2000
 
 int main(int argc, char *argv[]) {
 
-	int client_fd = open_clientfd(argv[1], argv[2]);
-	printf("%d\n", client_fd);
-	return 0;
+  int client_fd;
+  char *host;
+  char *port;
+  char buf[MAXLINE];
+	char server_reply[MAXLINE];
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+    exit(0);
+  }
+
+	host = argv[1];
+	port = argv[2];
+
+  if ((client_fd = open_clientfd(argv[1], argv[2])) == -1) {
+		fputs("Could not connect to server", stderr);
+		exit(0);
+	};
+
+	puts("Connected to server");
+
+	while (fgets(buf, MAXLINE, stdin) != NULL) {
+		if (send(client_fd, buf, MAXLINE, 0) < 0) {
+			puts("send error");
+			break;
+		};
+
+		memset(buf, 0, MAXLINE);
+		if (recv(client_fd, buf, MAXLINE, 0) < 0) {
+			fputs("recv failed.\n", stderr);
+			break;
+		};
+
+		fputs(buf, stdout);
+
+	}
+
+  close(client_fd);
+  return 0;
 }
--- server_main.c
+++ server_main.c
@@ -1,48 +1,57 @@
 #include "server/server_socket.h"
+#include "server/echo.h"
+
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netdb.h>
 #include <unistd.h>
 
 #define MAXLINE 2000
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-	// Declaring variables
-	int listen_fd;
-	int conn_fd;
-	socklen_t clientlen;
-	char client_hostname[MAXLINE];
-	char client_port[MAXLINE];
+  // Declaring variables
+  int listen_fd;
+  int conn_fd;
+  socklen_t clientlen;
+  char client_hostname[MAXLINE];
+  char client_port[MAXLINE];
+  int read_size;
 
-	// using sockaddr_storage makes sure we have space for any address
-	struct sockaddr_storage clientaddr; 
+  // using sockaddr_storage makes sure we have space for any address
+  struct sockaddr_storage clientaddr;
 
-	clientlen = sizeof(struct sockaddr_storage);
-	// Port number to be listened to must be specified
-	if (argc != 2)
-	{
-		fprintf(stderr, "usage: %s <port>\n", argv[0]);
-		exit(0);
-	}
-	
-	listen_fd = open_listenfd(argv[1]);
-	puts("Listening for incoming connections");
+  clientlen = sizeof(struct sockaddr_storage);
+  // Port number to be listened to must be specified
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
 
-//	while (1) {
-		// Accept incoming connection and place client sockaddr in clientaddr
-		conn_fd = accept(listen_fd, (struct sockaddr *)&clientaddr, &clientlen);
+  listen_fd = open_listenfd(argv[1]);
+  if (listen_fd == -1) {
+    puts("Could not start server");
+    exit(0);
+  }
 
-		// retreiving client hostname and port from clientaddr
-		getnameinfo((struct sockaddr *)&clientaddr, clientlen, client_hostname, MAXLINE, 
-						client_port, MAXLINE, 0);
-		
-		printf("Connected to (%s, %s)", client_hostname, client_port);
+  while (1) {
 
-		close(conn_fd);
-//	}
+		puts("Listening for incoming connections...\n");
+    // Accept incoming connection and place client sockaddr in clientaddr
+    conn_fd = accept(listen_fd, (struct sockaddr *)&clientaddr, &clientlen);
 
+    // retreiving client hostname and port from clientaddr
+    getnameinfo((struct sockaddr *)&clientaddr, clientlen, client_hostname,
+                MAXLINE, client_port, MAXLINE, 0);
 
-	return 0; 
+    printf("Connected to (%s, %s)\n", client_hostname, client_port);
+
+		echo(conn_fd);
+
+		puts("Closing connection");
+    close(conn_fd);
+  }
+
+  return 0;
 }
